9675cd402d4f13c529bab048c1b6a71a05462c01
impelcrypto <impelcrypto@gmail.com>
2025-04-25 23:03:35 +0800

    feat: updated optin contract

--- contracts/OptIn/OptIn.sol
+++ contracts/OptIn/OptIn.sol
@@ -1,26 +1,42 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OptIn is ERC721, Ownable {
-    uint256 private counter;
+contract ScoreOptIn is ERC721, Ownable {
+    uint256 public _nextTokenId;
 
     constructor(
         address initialOwner
-    ) ERC721("OptIn", "SOI") Ownable(initialOwner) {
-        transferOwnership(initialOwner);
-        counter = 0;
+    ) ERC721("ScoreOptIn", "SOI") Ownable(initialOwner) {
+        _nextTokenId = 1;
     }
 
-    function optIn(address to) public onlyOwner {
+    function optIn(address to) public returns (uint256) {
         require(balanceOf(to) == 0, "Recipient already owns an NFT");
-        counter++;
-        _safeMint(to, counter);
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        return tokenId;
     }
 
-    function getCounter() public view returns (uint256) {
-        return counter;
+    /**
+     * @dev Override the _update function to block transfers
+     * This makes the token non-transferable except during initial minting
+     */
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override returns (address) {
+        address from = _ownerOf(tokenId);
+
+        // Allow minting (when from is zero address) but block transfers
+        if (from != address(0)) {
+            revert("Token transfer is BLOCKED");
+        }
+
+        return super._update(to, tokenId, auth);
     }
 }
